fd1b3609cb10d4bb857cface271d366eadcf0c20
Denis Semenov <denissemo1999@gmail.com>
2020-11-04 19:50:42 +0200

    Init. Basic project configuration.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `
+        <h1>Working</h1>
+    `,
+    styles: []
+})
+export class AppComponent {
+    title = 'go-todo-app';
+}
